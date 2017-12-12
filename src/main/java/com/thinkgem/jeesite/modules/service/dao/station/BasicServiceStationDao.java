a37be4fa31edc8eb4f3f309c6a3c5ec37f1a8963
X <13693219511@163.com>
2017-12-12 09:01:02 +0800

    服务站

---
+++ src/main/java/com/thinkgem/jeesite/modules/service/dao/station/BasicServiceStationDao.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.service.dao.station;
+
+import com.thinkgem.jeesite.common.persistence.CrudDao;
+import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
+import com.thinkgem.jeesite.modules.service.entity.station.BasicServiceStation;
+
+/**
+ * 服务站DAO接口
+ * @author 服务站
+ * @version 2017-12-11
+ */
+@MyBatisDao
+public interface BasicServiceStationDao extends CrudDao<BasicServiceStation> {
+	
+}
