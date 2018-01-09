3dcea3ccad66a1d8cda0cdb50218d1a6c1a8b0b7
wangli <jixson@foxmail.com>
2018-01-09 09:23:42 +0800

    ReqResAop类里拦截参数处理

--- src/main/java/com/thinkgem/jeesite/common/aspect/ReqResAop.java
+++ src/main/java/com/thinkgem/jeesite/common/aspect/ReqResAop.java
@@ -2,6 +2,10 @@ package com.thinkgem.jeesite.common.aspect;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thinkgem.jeesite.common.mapper.JsonMapper;
+import com.thoughtworks.xstream.mapper.Mapper.Null;
+
+import java.net.URLDecoder;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
@@ -15,30 +19,41 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Aspect
 @EnableAspectJAutoProxy
 public class ReqResAop {
-    Log logger = LogFactory.getLog(getClass());
+	Log logger = LogFactory.getLog(getClass());
 
-    @Pointcut("within(@org.springframework.stereotype.Controller *)")
-    public void point() {
-        logger.info("-----------------------------------------");
-    }
+	@Pointcut("within(@org.springframework.stereotype.Controller *)")
+	public void point() {
+		logger.info("-----------------------------------------");
+	}
 
-    @Before("point()")
-    public void before(JoinPoint jp) throws Exception {
-    }
+	// com.thinkgem.jeesite.modules.service.web.sort
+	@Before("execution(* com.thinkgem..web..*.*(..))")
+	public void before(JoinPoint jp) throws Exception {
+		for (int i = 0; i < jp.getArgs().length; i++) {
+			System.out.println("ReqResAop类拦截器处理请求参数===========" + jp.getArgs()[i]);
+			if (jp.getArgs()[i] != null) {
+				String string = jp.getArgs()[i].toString();
+				System.out.println("转换前的参数=============" + string);
+				String decode = URLDecoder.decode(string, "UTF-8");
+				System.out.println("转换后的参数=============" + decode);
+			}
+		}
 
-    @Around("point()")
-    public Object around(ProceedingJoinPoint jp) throws Throwable {
-        Object proceed = jp.proceed();
-        logger.info("返回值：" + JsonMapper.toJsonString(proceed));
-        return proceed;
-    }
+	}
 
-    @AfterReturning("point()")
-    public void afterReturning(JoinPoint jp) throws Exception {
-    }
+	@Around("point()")
+	public Object around(ProceedingJoinPoint jp) throws Throwable {
+		Object proceed = jp.proceed();
+		logger.info("返回值：" + JsonMapper.toJsonString(proceed));
+		return proceed;
+	}
 
-    @AfterThrowing(value = "point()", throwing = "ex")
-    public void doExceptionActions(JoinPoint jp, Throwable ex) {
+	@AfterReturning("point()")
+	public void afterReturning(JoinPoint jp) throws Exception {
+	}
 
-    }
+	@AfterThrowing(value = "point()", throwing = "ex")
+	public void doExceptionActions(JoinPoint jp, Throwable ex) {
+
+	}
 }
