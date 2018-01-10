77a26107062bdd63e2a37ac92e09d3b5b390eb88
WYR <m13994888329@163.com>
2018-01-10 17:11:45 +0800

    测试git提交

--- src/main/java/com/thinkgem/jeesite/common/persistence/BaseEntity.java
+++ src/main/java/com/thinkgem/jeesite/common/persistence/BaseEntity.java
@@ -31,7 +31,7 @@ public abstract class BaseEntity<T>implements Serializable {
 	  private static final long serialVersionUID = 1L;
       
 	/**
-	 * 实体编号（唯一标识）
+	 * 实体编号（唯一标识）id
 	 */       
 	protected String id;
 	
