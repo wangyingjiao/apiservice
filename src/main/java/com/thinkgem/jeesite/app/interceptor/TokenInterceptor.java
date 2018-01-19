8767047b9971ee1816735f0d7159b6fa530cc626
wangyingjiao <875541991@qq.com>
2018-01-19 18:16:32 +0800

    app token

--- src/main/java/com/thinkgem/jeesite/app/interceptor/TokenInterceptor.java
+++ src/main/java/com/thinkgem/jeesite/app/interceptor/TokenInterceptor.java
@@ -7,7 +7,6 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author x
  */
 
-public class TokenInterceptor  extends HandlerInterceptorAdapter {
+public class TokenInterceptor implements HandlerInterceptor {
 
     private Log loger = LogFactory.getLog(getClass());
 
@@ -41,19 +40,18 @@ public class TokenInterceptor  extends HandlerInterceptorAdapter {
         loger.info("拦截器处理==>  进入app拦截处理");
         String header = request.getHeader(TOKEN);
         loger.debug("token 值为 ==>" + header);
-//        Token token = tokenManager.verifyToken(new Token(header));
-        return true;
-//        if (null == token) {
-//            response.getWriter().print(JSON.toJSONString(new FailResult<>("token 无效或 已经过期，请重新登录")));
-//            response.getWriter().flush();
-//            loger.info("token 无效或 已经过期，请重新登录" + header);
-//            return false;
-//        } else {
-//            request.setAttribute("token", token);
-//            loger.debug("==> token 正常,刷新token 时间 "+token.toString());
-//            tokenManager.updateToken(token);
-//            return true;
-//        }
+        Token token = tokenManager.verifyToken(new Token(header));
+        if (null == token) {
+            response.getWriter().print(JSON.toJSONString(new FailResult<>("token 无效或 已经过期，请重新登录")));
+            response.getWriter().flush();
+            loger.info("token 无效或 已经过期，请重新登录" + header);
+            return false;
+        } else {
+            request.setAttribute("token", token);
+            loger.debug("==> token 正常,刷新token 时间 "+token.toString());
+            tokenManager.updateToken(token);
+            return true;
+        }
     }
 
     @Override
