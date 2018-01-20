6f5432215f25e174094a7cf3579b9cfdda2c9543
wangyarong <13994888329@163.com>
2018-01-20 11:32:00 +0800

    提交保存代码bug 13325、13053,延迟解决，当前登录账号编辑、新增岗位权限的问题

--- src/main/java/com/thinkgem/jeesite/modules/sys/web/RoleController.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/web/RoleController.java
@@ -509,6 +509,7 @@ public class RoleController extends BaseController {
 	@ApiOperation(value = "得到当前用户能看到的（默认当前机构）角色列表")
 	public Result listPageData(@RequestBody(required = false) Role role, HttpServletRequest request,
 			HttpServletResponse response) {
+		@SuppressWarnings("rawtypes")
 		Page<Role> page = new Page<>(request, response);
 		Page<Role> list = systemService.findRole(page, role);
 		return new SuccResult(list);
@@ -547,25 +548,51 @@ public class RoleController extends BaseController {
 		} else {
 			role.setFlag(false);
 		}
-		/*//add by wyr:获取apiservice/a/sys/menu/getMenuList的岗位集合
-		List<Menu> menuList = UserUtils.getMenuListForPlatform();
-		List<Menu> menus = genTreeMenu("1", menuList);//A
+		//方案一
+		/*List<Menu> menuList = UserUtils.getMenuListForPlatform();
+		List<Menu> menuRoleList = role.getMenuList();
+		menuRoleList.removeAll(menuList);
+		List<Menu> menusRoleDis = genTreeMenusUnion("1", menuRoleList);
+		List<Menu> menus = genTreeMenu("1", menuList);
+		menus.addAll(menusRoleDis);*/
+		//方案二
+		//add by wyr:获取apiservice/a/sys/menu/getMenuList的岗位集合
+		/*List<Menu> menuList = UserUtils.getMenuListForPlatform();
+		List<Menu> menus = genTreeMenu("1", menuList);
 		
 		List<Menu> menuRoleList = role.getMenuList();
-		List<Menu> menusRole = genTreeMenu("1", menuRoleList);//B
+		List<Menu> menusRole = genTreeMenu("1", menuRoleList);
 		
 		//与结果role的岗位集合menuRoleList去重取并集
 		menusRole.removeAll(menus);
+		//List<Menu> menusRoleDis = genTreeMenusUnion("1", menusRole);
 		for (Menu menu : menusRole) {
 			menu.setDisable(true);
 		}
-		menus.addAll(menusRole);
-		role.setMenuListUnion(menus);*/
+		menus.addAll(menusRole);*/
+		
+		//role.setMenuListUnion(menus);
 		
 		if (role != null) {
 			return new SuccResult(role);
 		}
 		return new FailResult("岗位信息未找到");
 	}
+	
+	
+	private  List<Menu> genTreeMenusUnion(String id, List<Menu> menusRole) {
+        ArrayList<Menu> list = new ArrayList<>();
+        for (Menu menu : menusRole) {
+        	menu.setDisable(true);
+            //如果对象的父id等于传进来的id，则进行递归，进入下一轮；
+            if (menu.getParentId().equals(id)) {
+                List<Menu> menus1 = genTreeMenusUnion(menu.getId(), menusRole);
+                menu.setSubMenus(menus1);
+                menu.setDisable(true);
+                list.add(menu);
+            }
+        }
+        return list;
+    } 
 
 }
