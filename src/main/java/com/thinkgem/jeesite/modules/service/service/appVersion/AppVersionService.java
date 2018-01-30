a58633d888a8a76ff59f5912c219b96ab9384fa2
huamingjiang_0311 <huamj03@163.com>
2018-01-30 13:23:13 +0800

    新增APP发版增删改查接口

---
+++ src/main/java/com/thinkgem/jeesite/modules/service/service/appVersion/AppVersionService.java
@@ -0,0 +1,48 @@
+package com.thinkgem.jeesite.modules.service.service.appVersion;
+
+import com.thinkgem.jeesite.common.persistence.Page;
+import com.thinkgem.jeesite.common.service.CrudService;
+import com.thinkgem.jeesite.common.utils.IdGen;
+import com.thinkgem.jeesite.modules.service.dao.appVersion.AppVersionDao;
+import com.thinkgem.jeesite.modules.service.entity.appVersion.AppVersion;
+import com.thinkgem.jeesite.modules.sys.utils.UserUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+/**
+ * APP发版管理Service
+ * @author a
+ * @version 2018-01-29
+ */
+@Service
+@Transactional(readOnly = true)
+public class AppVersionService extends CrudService<AppVersionDao,AppVersion> {
+    @Autowired
+    private AppVersionDao appVersionDao;
+
+    public Page<AppVersion> findPage(Page<AppVersion> page, AppVersion appVersion) {
+        //appVersion.getSqlMap().put("dsf", dataRoleFilter(UserUtils.getUser(), "a"));
+        return super.findPage(page, appVersion);
+    }
+
+    @Transactional(readOnly = false)
+    public void save(AppVersion appVersion) {
+            appVersion.setUpdateBy(UserUtils.getUser());
+            appVersion.setUpdateDate(new Date());
+            super.save(appVersion);
+    }
+
+    public AppVersion getData(String id) {
+        AppVersion appVersion = super.get(id);
+        return appVersion;
+    }
+
+    @Transactional(readOnly = false)
+    public void delete(AppVersion appVersion) {
+        super.delete(appVersion);
+    }
+
+}
