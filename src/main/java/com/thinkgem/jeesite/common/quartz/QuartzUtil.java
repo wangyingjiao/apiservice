493b55c1bd1945fcde44fdc856d849ea85ecba62
lizhen <965177809@qq.com>
2018-03-22 14:28:34 +0800

    定时器test

--- src/main/java/com/thinkgem/jeesite/common/quartz/QuartzUtil.java
+++ src/main/java/com/thinkgem/jeesite/common/quartz/QuartzUtil.java
@@ -8,6 +8,10 @@ public class QuartzUtil {
     @Autowired
     SendQuartzService sendQuartzService;
 
+    public QuartzUtil(){
+        System.out.println("计划任务被初始化了");
+    }
+
     public void doJointWait(){
         System.out.println("定时任务对接开始");
         sendQuartzService.doJointWait();
