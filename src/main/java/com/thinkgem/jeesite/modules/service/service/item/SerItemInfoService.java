78078b9aee8bd20209f4a6eb461db2f1a4fd5030
wangyingjiao <875541991@qq.com>
2018-03-23 16:58:35 +0800

    对接商品列表

--- src/main/java/com/thinkgem/jeesite/modules/service/service/item/SerItemInfoService.java
+++ src/main/java/com/thinkgem/jeesite/modules/service/service/item/SerItemInfoService.java
@@ -679,10 +679,14 @@ public class SerItemInfoService extends CrudService<SerItemInfoDao, SerItemInfo>
 		//将对接编码通过_截取 赋值给商品id
 		String selfCode = serItemCommodityEshop.getSelfCode();
 		if (StringUtils.isNotBlank(selfCode)) {
-			String[] split = selfCode.split("_");
-			if (split.length>1) {
-				String goodsId = split[1];
-				serItemCommodityEshop.setGoodsId(goodsId);
+			if (selfCode.contains("_")) {
+				String[] split = selfCode.split("_");
+				if (split.length > 1) {
+					String goodsId = split[1];
+					serItemCommodityEshop.setGoodsId(goodsId);
+				}
+			}else {
+				serItemCommodityEshop.setGoodsId(selfCode);
 			}
 		}
 		serItemCommodityEshop.getSqlMap().put("dsf", dataRoleFilter(UserUtils.getUser(), "a"));
