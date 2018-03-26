14ef5d28395d5c8f3416b84c48303b9d8c4463e5
wangyingjiao <875541991@qq.com>
2018-03-26 14:51:08 +0800

    日志模块

---
+++ src/main/java/com/thinkgem/jeesite/common/filter/HttpServletRequestReplacedFilter.java
@@ -0,0 +1,46 @@
+package com.thinkgem.jeesite.common.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class HttpServletRequestReplacedFilter implements Filter {
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        ServletRequest requestWrapper = null;
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+            System.out.println(httpServletRequest.getContentType());
+            System.out.println(httpServletRequest.getContextPath());
+            System.out.println(httpServletRequest.getRemoteHost());
+            System.out.println(httpServletRequest.getMethod());
+            if ("POST".equals(httpServletRequest.getMethod().toUpperCase())
+                    ) {
+                //&& httpServletRequest.getContentType().equalsIgnoreCase(
+                // "application/json")
+                requestWrapper = new BodyReaderHttpServletRequestWrapper(
+                        (HttpServletRequest) request);
+            }
+        }
+
+        if (requestWrapper == null) {
+            chain.doFilter(request, response);
+        } else {
+            chain.doFilter(requestWrapper, response);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+
+    }
+
+}
