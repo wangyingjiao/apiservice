931b08034571eb91d2802dde6fae1bc7cf5f37e6
lizhen <965177809@qq.com>
2018-03-29 17:19:59 +0800

    test

---
+++ src/main/java/com/thinkgem/jeesite/common/redis/JedisConstant.java
@@ -0,0 +1,8 @@
+package com.thinkgem.jeesite.common.redis;
+
+public class JedisConstant {
+
+    public static final String CACHE_NEWEST_VERSION = "newestVersion";
+    public static final String REDIS_PASSWORD = "redisPwd";
+
+}
