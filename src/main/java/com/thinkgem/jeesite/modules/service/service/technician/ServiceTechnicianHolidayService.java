c703012f685a2b56a79e570a81f996b3d651fd07
wangyingjiao <875541991@qq.com>
2018-04-03 16:52:32 +0800

    app 修改订单状态bug

--- src/main/java/com/thinkgem/jeesite/modules/service/service/technician/ServiceTechnicianHolidayService.java
+++ src/main/java/com/thinkgem/jeesite/modules/service/service/technician/ServiceTechnicianHolidayService.java
@@ -191,8 +191,12 @@ public class ServiceTechnicianHolidayService extends CrudService<ServiceTechnici
 		info.setId(serviceTechnicianHoliday.getTechId());
 		ServiceTechnicianInfo serviceTechnicianInfo = serviceTechnicianInfoDao.appFindTech(info);
 		String jobStatus = serviceTechnicianInfo.getJobStatus();
+		String jobNature = serviceTechnicianInfo.getJobNature();
 		if (StringUtils.isNotBlank(jobStatus) && "leave".equals(jobStatus)){
 			throw new ServiceException("技师已经离职,不可请假");
+		}
+		if (StringUtils.isNotBlank(jobNature) && "part_time".equals(jobNature)){
+			throw new ServiceException("兼职技师,不可请假");
 		}
 		//转存对象为排期表
 		TechScheduleInfo techScheduleInfo=new TechScheduleInfo();
